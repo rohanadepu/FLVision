129b017a18a062a7672e67ede2325e5b33d6e8b8
rohanadepu <45407301+rohanadepu@users.noreply.github.com>
2024-05-30 13:45:25 -0500

    add demo files for FL

---
+++ Demo/client.py
@@ -0,0 +1,28 @@
+import os
+import flwr as fl
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+model.compile(optimizer='adam', 
+              loss=tf.keras.losses.sparse_categorical_crossentropy, 
+              metrics=['accuracy'])
+
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=3)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": float(accuracy)}
+
+fl.client.start_client(server_address="localhost:8080", client=CifarClient().to_client())
