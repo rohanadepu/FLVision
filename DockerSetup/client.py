66bd02155004349d96b8de244393bfa2f8f89150
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-06-09 15:11:10 -0400

    aerpaw implementation

--- DockerSetup/client.py
+++ DockerSetup/client.py
@@ -479,4 +479,4 @@ class FLClient(fl.client.NumPyClient):
 #    Start the client                                   #
 #########################################################
 
-fl.client.start_client(server_address="192.168.111.2:8080", client=FLClient().to_client())
+fl.client.start_client(server_address="192.168.117.3:8080", client=FLClient().to_client())
