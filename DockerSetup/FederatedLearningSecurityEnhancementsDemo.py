c2ad6fb449674601bc89f8a76096beec5db69edf
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-06-19 22:05:07 -0400

    secure aggregation + homogenous encryption

--- DockerSetup/FederatedLearningSecurityEnhancementsDemo.py
+++ DockerSetup/FederatedLearningSecurityEnhancementsDemo.py
@@ -570,7 +570,7 @@ if dataset_used == "IOTBOTNET":
 # Set the privacy parameters
 noise_multiplier = 1.1
 l2_norm_clip = 1.0
-num_microbatches = 1
+num_microbatches = 1 # this is bugged
 batch_size = 32
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
