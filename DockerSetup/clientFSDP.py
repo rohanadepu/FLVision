94976c91ddbc52f8f7bd333ea9a0f20d7a0c9fab
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-06-24 17:01:10 -0400

    hyperparameter engineering, DNN egineering, including callback, additional formatting

--- DockerSetup/clientFSDP.py
+++ DockerSetup/clientFSDP.py
@@ -819,7 +819,7 @@ if dataset_used == "IOTBOTNET":
     l2_alpha = 0.01  # Increase if overfitting, decrease if underfitting
 
     epochs = 5  # will be optimized
-    steps_per_epoch = len(X_train_data) // batch_size  # dependant
+    steps_per_epoch = (len(X_train_data) // batch_size) // epochs  # dependant
 
     input_dim = X_train_data.shape[1]  # dependant
 
