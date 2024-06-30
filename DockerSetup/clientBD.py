529ce178c7b47c62faaf87f3375daa04f7025528
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-06-29 21:09:58 -0400

    stabilizing model with balanced data

--- DockerSetup/clientBD.py
+++ DockerSetup/clientBD.py
@@ -630,7 +630,7 @@ model_name = dataset_used  # name for file
 
 noise_multiplier = 0.1  # Privacy param - noise budget: 0, none; 1, some noise; >1, more noise
 
-l2_norm_clip = 10.0  # privacy param: 0.1 - 10: larger value, larger gradients, smaller value, more clipping
+l2_norm_clip = 7.5  # privacy param: 0.1 - 10: larger value, larger gradients, smaller value, more clipping
 
 batch_size = 64  # 32 - 128; try 64, 96, 128; maybe intervals of 16
 num_microbatches = 1  # this is bugged keep at 1
