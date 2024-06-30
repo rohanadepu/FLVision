875ae10e2b3a30f2b62b78d1edee02e9e9adba73
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-06-29 20:32:55 -0400

    stabilizing model with balanced data

--- DockerSetup/clientBD.py
+++ DockerSetup/clientBD.py
@@ -685,9 +685,9 @@ if dataset_used == "CIFAR":
 
 model_name = dataset_used  # name for file
 
-noise_multiplier = 0.9  # Privacy param - noise budget: 0, none; 1, some noise; >1, more noise
+noise_multiplier = 0.5  # Privacy param - noise budget: 0, none; 1, some noise; >1, more noise
 
-l2_norm_clip = 2.0  # privacy param: 0.1 - 10: larger value, larger gradients, smaller value, more clipping
+l2_norm_clip = 5.0  # privacy param: 0.1 - 10: larger value, larger gradients, smaller value, more clipping
 
 batch_size = 64  # 32 - 128; try 64, 96, 128; maybe intervals of 16
 num_microbatches = 1  # this is bugged keep at 1
@@ -697,8 +697,8 @@ betas = [0.9, 0.999]  # Best to keep as is
 l2_alpha = 0.01  # Increase if overfitting, decrease if underfitting
 
 epochs = 5  # will be optimized
-steps_per_epoch = (len(X_train_data) // batch_size) // epochs  # dependant
-# steps_per_epoch = (len(X_train_data) // batch_size)   # dependant # debug
+# steps_per_epoch = (len(X_train_data) // batch_size) // epochs  # dependant
+steps_per_epoch = (len(X_train_data) // batch_size)   # dependant # debug
 
 input_dim = X_train_data.shape[1]  # dependant
 
