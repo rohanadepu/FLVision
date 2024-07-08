40da96a3f55dbcdebd6c0c2e30a547ef58ffaf27
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-07-08 14:13:01 -0400

    implementing new defenses model prune, fgsm basic adversarial training, sec agg plus, put dependencies in conditions to ensure stability

--- ExperimentPrograms/server.py
+++ ExperimentPrograms/server.py
@@ -2,19 +2,46 @@
 #    Imports / Env setup                                #
 #########################################################
 
+import argparse
 import flwr as fl
 
+#########################################################
+#    Script Args                                        #
+#########################################################
+
+parser = argparse.ArgumentParser(description='Select to enabled sec agg')
+parser.add_argument('--secAggP', action='store_true', help='Enable model adversarial training')
+
+args = parser.parse_args()
+
+secAggPlusEnabled = args.secAggP
+
+if secAggPlusEnabled:
+    print("Secure Aggregation Plus Enabled \n")
+else:
+    print("Secure Aggregation Plus Disabled \n")
 #########################################################
 #    Setup Server                  #
 #########################################################
 
-# local model pruning
+if secAggPlusEnabled:
+
+    # Define the server configuration with SecAgg+ enabled
+    secagg_config = fl.server.SecAggConfig(
+        secagg_type="SecAgg+",  # Enable SecAgg+
+        threshold_ratio=0.5,  # Define the threshold ratio for SecAgg+
+    )
+
+    # Start the server with the defined configuration
+    fl.server.start_server(
+        config=fl.server.ServerConfig(num_rounds=10, secagg_config=secagg_config)
+    )
 
 
 #########################################################
 #    Initialize Server                  #
 #########################################################
-
-fl.server.start_server(
-    config=fl.server.ServerConfig(num_rounds=10)
-)
+else:
+    fl.server.start_server(
+        config=fl.server.ServerConfig(num_rounds=10)
+    )
