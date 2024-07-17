a7152a8011746ad71fab908fe16afaf46df61463
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-07-17 01:26:03 -0400

    corrected label flipping for ciciot

--- poisoningmulti.py
+++ poisoningmulti.py
@@ -21,9 +21,18 @@ dict_2classes = {'DDoS-RSTFINFlood': 'Attack', 'DDoS-PSHACK_Flood': 'Attack', 'D
                      'DictionaryBruteForce': 'Attack'
                     }
 
+def remap_labels(df, label):
+    if label in df.columns:
+        df[label] = df[label].map(dict_2classes).fillna(df[label])
+    return df
+
 
 def poison_dataset(file_path, output_folder, label):
     df = pd.read_csv(file_path)
+
+    # Remap labels if it's the CICIOT dataset
+    if 'CICIOT2023' in file_path:
+        df = remap_labels(df, label)
     
     # Assuming we want to poison 100% of the selected files using the least frequent label
     least_frequent_label = df[label].value_counts().idxmin()
@@ -48,26 +57,26 @@ def handle_file(file_path, output_folder, label, poison_probability):
         os.makedirs(new_output_folder, exist_ok=True)
         shutil.copy(file_path, os.path.join(new_output_folder, os.path.basename(file_path)))
 
+
 def poison_all_files(input_folder, poison_percentage):
-
-
     label = 'label' if 'CICIOT2023' in input_folder else 'Label'
     output_folder = input_folder + '_POISONED'
 
-    #if CICIOT remap labels to dict_2classes
-    
     # Ensure the output directory does not exist to start fresh
     if os.path.exists(output_folder):
         shutil.rmtree(output_folder)
-    
-    csv_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(input_folder) for f in filenames if f.endswith('.csv')]
+
+    csv_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(input_folder) for f in filenames if
+                 f.endswith('.csv')]
     poison_probability = poison_percentage / 100
 
     # Use a number of threads based on the number of CPUs
     num_threads = multiprocessing.cpu_count()
 
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        executor.map(handle_file, csv_files, [output_folder]*len(csv_files), [label]*len(csv_files), [poison_probability]*len(csv_files))
+        executor.map(handle_file, csv_files, [output_folder] * len(csv_files), [label] * len(csv_files),
+                     [poison_probability] * len(csv_files))
+
 
 # Example usage
 input_folder = 'CICIOT2023'  # or 'CICIOT2023'
