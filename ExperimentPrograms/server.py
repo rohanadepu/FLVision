11a924fb1d9ec0d75fb5930a6753faf35be3b1d4
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-07-17 01:39:39 -0400

    new round deafult to 8 and added input as script argument range 1-10

--- ExperimentPrograms/server.py
+++ ExperimentPrograms/server.py
@@ -3,11 +3,23 @@
 #########################################################
 
 import flwr as fl
+import argparse
 
 #########################################################
-#    Initialize Server                  #
+#    Config Server                                      #
+#########################################################
+
+parser = argparse.ArgumentParser(description="Federated Learning Training Script Server Side, --rounds [1-10] to select rounds")
+parser.add_argument("--rounds", type=int, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], default=8, help="Rounds of training 1-10")
+
+args = parser.parse_args()
+
+roundInput = args.rounds
+
+#########################################################
+#    Initialize Server                                  #
 #########################################################
 
 fl.server.start_server(
-    config=fl.server.ServerConfig(num_rounds=10)
+    config=fl.server.ServerConfig(num_rounds=roundInput)
 )
