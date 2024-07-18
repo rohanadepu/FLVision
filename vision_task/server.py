eb734685b8d27427aa05b6769d7331f38565ce21
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-07-17 23:09:57 -0400

    vision task + flypaw + anomally detection

--- vision_task/server.py
+++ vision_task/server.py
@@ -4,10 +4,7 @@ import flwr as fl
 
 def main():
     fl.server.start_server(
-        server_address="0.0.0.0:8080",
-        config=fl.server.ServerConfig(
-            num_rounds=10
-        )
+        config=fl.server.ServerConfig(num_rounds=8)
     )
 
 
