37a29e1782dc15e3fd3a0c743893d4492e28cf38
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-08-07 18:06:25 -0400

    results

--- ExperimentPrograms/server.py
+++ ExperimentPrograms/server.py
@@ -11,15 +11,20 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Federated Learning Training Script Server Side, --rounds [1-10] to select rounds")
 parser.add_argument("--rounds", type=int, choices=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], default=8, help="Rounds of training 1-10")
+parser.add_argument("--min_clients", type=int, choices=[1, 2, 3, 4, 5, 6], default=2, help="Minimum number of clients required for training")
 
 args = parser.parse_args()
 
 roundInput = args.rounds
+minClients = args.min_clients
 
 #########################################################
 #    Initialize Server                                  #
 #########################################################
 
 fl.server.start_server(
-    config=fl.server.ServerConfig(num_rounds=roundInput)
+    config=fl.server.ServerConfig(num_rounds=roundInput),
+    strategy=fl.server.strategy.FedAvg(
+        min_available_clients=minClients
+    )
 )
