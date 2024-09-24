c0d2652f62025eb837767b975f86cbe7a0da4393
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2024-09-24 13:47:24 -0400

    results

---
+++ gan/ciciotDatasetLoad.py
@@ -0,0 +1,272 @@
+#########################################################
+#    Imports / Env setup                                #
+#########################################################
+
+import os
+import random
+import time
+from datetime import datetime
+import argparse
+
+if 'TF_USE_LEGACY_KERAS' in os.environ:
+    del os.environ['TF_USE_LEGACY_KERAS']
+
+import flwr as fl
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, BatchNormalization, Dropout
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.metrics import AUC, Precision, Recall
+from tensorflow.keras.losses import LogCosh
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from numpy import expand_dims
+
+# import math
+# import glob
+
+# from tqdm import tqdm
+
+# import seaborn as sns
+
+# import pickle
+# import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, RobustScaler, PowerTransformer, LabelEncoder, MinMaxScaler
+from sklearn.utils import shuffle
+
+
+##########################################################################################
+#                          Load Config for CICIOT 2023 Dataset                           #
+##########################################################################################
+def loadCICIOT(poisonedDataType=None):
+    # ---                   Data Loading Settings                     --- #
+
+    # Sample size for train and test datasets
+    ciciot_train_sample_size = 25  # input: 3 samples for training
+    ciciot_test_sample_size = 10  # input: 1 sample for testing
+
+    # label classes 33+1 7+1 1+1
+    ciciot_label_class = "1+1"
+
+    if poisonedDataType == "LF33":
+        DATASET_DIRECTORY = '/root/datasets/CICIOT2023_POISONEDLF33'
+
+    elif poisonedDataType == "LF66":
+        DATASET_DIRECTORY = '/root/datasets/CICIOT2023_POISONEDLF66'
+
+    elif poisonedDataType == "FN33":
+        DATASET_DIRECTORY = '/root/datasets/CICIOT2023_POISONEDFN33'
+
+    elif poisonedDataType == "FN66":
+        DATASET_DIRECTORY = '/root/datasets/CICIOT2023_POISONEDFN66'
+
+    else:
+        # directory of the stored data samples
+        DATASET_DIRECTORY = '../../datasets/CICIOT2023'
+
+    # ---    CICIOT Feature Mapping for numerical and categorical features       --- #
+
+    num_cols = ['flow_duration', 'Header_Length', 'Rate', 'Srate', 'Drate', 'ack_count', 'syn_count', 'fin_count',
+                'urg_count', 'rst_count', 'Tot sum', 'Min', 'Max', 'AVG', 'Std', 'Tot size', 'IAT', 'Number',
+                'Magnitue', 'Radius', 'Covariance', 'Variance', 'Weight'
+                ]
+
+    cat_cols = [
+        'Protocol Type', 'Duration', 'fin flag number', 'syn flag number', 'rst flag number', 'psh flag number',
+        'ack flag number', 'ece flag number', 'cwr flag number', 'HTTP', 'HTTPS', 'DNS', 'Telnet', 'SMTP', 'SSH', 'IRC',
+        'TCP', 'UDP', 'DHCP', 'ARP', 'ICMP', 'IPv', 'LLC'
+    ]
+
+    # ---                   CICIOT irrelevant & relevant features mappings                    --- #
+
+    irrelevant_features = ['Srate', 'ece_flag_number', 'rst_flag_number', 'ack_flag_number', 'cwr_flag_number',
+                           'ack_count', 'syn_count', 'fin_count', 'rst_count', 'LLC', 'Min', 'Max', 'AVG', 'Std',
+                           'Tot size', 'Number', 'Magnitue', 'Radius', 'Covariance', 'Variance', 'Weight',
+                           'flow_duration', 'Header_Length', 'urg_count', 'Tot sum'
+                           ]
+
+    # select the num cols that are relevant
+    relevant_num_cols = [col for col in num_cols if col not in irrelevant_features]
+
+    ## Debug ##
+    # relevant_num_cols = [col for col in num_cols if col in relevant_features]
+    ## EOF DEBUG ##
+
+    # ---                   Label Mapping for 1+1 and 7+1                      --- #
+
+    dict_7classes = {'DDoS-RSTFINFlood': 'DDoS', 'DDoS-PSHACK_Flood': 'DDoS', 'DDoS-SYN_Flood': 'DDoS',
+                     'DDoS-UDP_Flood': 'DDoS', 'DDoS-TCP_Flood': 'DDoS', 'DDoS-ICMP_Flood': 'DDoS',
+                     'DDoS-SynonymousIP_Flood': 'DDoS', 'DDoS-ACK_Fragmentation': 'DDoS',
+                     'DDoS-UDP_Fragmentation': 'DDoS',
+                     'DDoS-ICMP_Fragmentation': 'DDoS', 'DDoS-SlowLoris': 'DDoS', 'DDoS-HTTP_Flood': 'DDoS',
+                     'DoS-UDP_Flood': 'DoS', 'DoS-SYN_Flood': 'DoS', 'DoS-TCP_Flood': 'DoS', 'DoS-HTTP_Flood': 'DoS',
+                     'Mirai-greeth_flood': 'Mirai', 'Mirai-greip_flood': 'Mirai', 'Mirai-udpplain': 'Mirai',
+                     'Recon-PingSweep': 'Recon', 'Recon-OSScan': 'Recon', 'Recon-PortScan': 'Recon',
+                     'VulnerabilityScan': 'Recon', 'Recon-HostDiscovery': 'Recon', 'DNS_Spoofing': 'Spoofing',
+                     'MITM-ArpSpoofing': 'Spoofing', 'BenignTraffic': 'Benign', 'BrowserHijacking': 'Web',
+                     'Backdoor_Malware': 'Web', 'XSS': 'Web', 'Uploading_Attack': 'Web', 'SqlInjection': 'Web',
+                     'CommandInjection': 'Web', 'DictionaryBruteForce': 'BruteForce'
+                     }
+
+    dict_2classes = {'DDoS-RSTFINFlood': 'Attack', 'DDoS-PSHACK_Flood': 'Attack', 'DDoS-SYN_Flood': 'Attack',
+                     'DDoS-UDP_Flood': 'Attack', 'DDoS-TCP_Flood': 'Attack', 'DDoS-ICMP_Flood': 'Attack',
+                     'DDoS-SynonymousIP_Flood': 'Attack', 'DDoS-ACK_Fragmentation': 'Attack',
+                     'DDoS-UDP_Fragmentation': 'Attack', 'DDoS-ICMP_Fragmentation': 'Attack',
+                     'DDoS-SlowLoris': 'Attack',
+                     'DDoS-HTTP_Flood': 'Attack', 'DoS-UDP_Flood': 'Attack', 'DoS-SYN_Flood': 'Attack',
+                     'DoS-TCP_Flood': 'Attack', 'DoS-HTTP_Flood': 'Attack', 'Mirai-greeth_flood': 'Attack',
+                     'Mirai-greip_flood': 'Attack', 'Mirai-udpplain': 'Attack', 'Recon-PingSweep': 'Attack',
+                     'Recon-OSScan': 'Attack', 'Recon-PortScan': 'Attack', 'VulnerabilityScan': 'Attack',
+                     'Recon-HostDiscovery': 'Attack', 'DNS_Spoofing': 'Attack', 'MITM-ArpSpoofing': 'Attack',
+                     'BenignTraffic': 'Benign', 'BrowserHijacking': 'Attack', 'Backdoor_Malware': 'Attack',
+                     'XSS': 'Attack',
+                     'Uploading_Attack': 'Attack', 'SqlInjection': 'Attack', 'CommandInjection': 'Attack',
+                     'DictionaryBruteForce': 'Attack'
+                     }
+
+    # ---      Functions    --- #
+
+    def load_and_balance_data(file_path, label_class_dict, current_benign_size, benign_size_limit):
+        # load data to dataframe
+        data = pd.read_csv(file_path)
+
+        # remap labels to new classification
+        data['label'] = data['label'].map(label_class_dict)
+
+        # Separate the classes
+        attack_samples = data[data['label'] == 'Attack']
+        benign_samples = data[data['label'] == 'Benign']
+
+        # Limit the benign samples to avoid overloading
+        remaining_benign_quota = benign_size_limit - current_benign_size
+        if len(benign_samples) > remaining_benign_quota:
+            benign_samples = benign_samples.sample(remaining_benign_quota, random_state=47)
+
+        # Balance the samples
+        min_samples = min(len(attack_samples), len(benign_samples))  # choosing the smallest number from both samples
+        if min_samples > 0:  # if min sample exist sample out the samples
+            attack_samples = attack_samples.sample(min_samples, random_state=47)
+            benign_samples = benign_samples.sample(min_samples, random_state=47)
+
+        # Bring both samples together
+        balanced_data = pd.concat([attack_samples, benign_samples])
+
+        return balanced_data, len(benign_samples)
+
+    def reduce_attack_samples(data, attack_ratio):
+        attack_samples = data[data['label'] == 'Attack']
+        benign_samples = data[data['label'] == 'Benign']
+
+        reduced_attack_samples = attack_samples.sample(frac=attack_ratio, random_state=47)
+        combined_data = pd.concat([benign_samples, reduced_attack_samples])
+
+        return combined_data
+
+    # ---     Load in two separate sets of file samples for the train and test datasets --- #
+
+    print("\nLoading Network Traffic Data Files...")
+
+    # List the files in the dataset
+    csv_filepaths = [filename for filename in os.listdir(DATASET_DIRECTORY) if filename.endswith('.csv')]
+    print(csv_filepaths)
+
+    # Randomly select the specified number of files for training and testing
+    if len(csv_filepaths) > (ciciot_train_sample_size + ciciot_test_sample_size):
+        train_sample_files = random.sample(csv_filepaths,
+                                           ciciot_train_sample_size)  # samples the file names from filepaths list
+        remaining_files = [file for file in csv_filepaths if
+                           file not in train_sample_files]  # takes the remaining files not from the training set
+        test_sample_files = random.sample(remaining_files,
+                                          ciciot_test_sample_size)  # samples from the remaining set of files
+    else:
+        # If there are not enough files, use all available files for training and testing
+        train_sample_files = csv_filepaths[:ciciot_train_sample_size]
+        test_sample_files = csv_filepaths[ciciot_train_sample_size:ciciot_train_sample_size + ciciot_test_sample_size]
+
+    # Sort the file paths (optional)
+    train_sample_files.sort()
+    test_sample_files.sort()
+
+    print("\nTraining Sets:\n", train_sample_files, "\n")
+    print("\nTest Sets:\n", test_sample_files, "\n")
+
+    # ---      Load the data from the sampled sets of files into train and test dataframes respectively    --- #
+
+    # Train Dataframe
+    ciciot_train_data = pd.DataFrame()
+    train_normal_traffic_total_size = 0
+    train_normal_traffic_size_limit = 110000
+
+    print("\nLoading Training Data...")
+    for data_set in train_sample_files:
+
+        # Load Data from sampled files until enough benign traffic is loaded
+        if train_normal_traffic_total_size >= train_normal_traffic_size_limit:
+            break
+
+        print(f"Training dataset sample {data_set} \n")
+
+        # find the path for sample
+        data_path = os.path.join(DATASET_DIRECTORY, data_set)
+
+        # load the dataset, remap, then balance
+        balanced_data, benign_count = load_and_balance_data(data_path, dict_2classes, train_normal_traffic_total_size,
+                                                            train_normal_traffic_size_limit)
+        train_normal_traffic_total_size += benign_count  # adding to quota count
+
+        print(
+            f"Benign Traffic Train Samples: {benign_count} | {train_normal_traffic_total_size} |LIMIT| {train_normal_traffic_size_limit}")
+
+        # add to train dataset
+        ciciot_train_data = pd.concat([ciciot_train_data, balanced_data])  # dataframe to manipulate
+
+    # Test Dataframe
+    ciciot_test_data = pd.DataFrame()
+    test_normal_traffic_total_size = 0
+    test_normal_traffic_size_limit = 44000
+    attack_ratio = 0.1  # Adjust this ratio as needed to achieve desired imbalance
+
+    print("\nLoading Testing Data...")
+    for data_set in test_sample_files:
+
+        # Load Data from sampled files until enough benign traffic is loaded
+        if test_normal_traffic_total_size >= test_normal_traffic_size_limit:
+            break
+
+        print(f"Testing dataset sample {data_set} \n")
+
+        # find the path for sample
+        data_path = os.path.join(DATASET_DIRECTORY, data_set)
+
+        # load the dataset, remap, then balance
+        balanced_data, benign_count = load_and_balance_data(data_path, dict_2classes, test_normal_traffic_total_size,
+                                                            test_normal_traffic_size_limit)
+
+        test_normal_traffic_total_size += benign_count  # adding to quota count
+
+        print(
+            f"Benign Traffic Test Samples: {benign_count} | {test_normal_traffic_total_size} |LIMIT| {test_normal_traffic_size_limit}")
+
+        # add to train dataset
+        ciciot_test_data = pd.concat([ciciot_test_data, balanced_data])  # dataframe to manipulate
+
+    # Reduce the number of attack samples in the test set
+    ciciot_test_data = reduce_attack_samples(ciciot_test_data, attack_ratio)
+
+    print("\nTrain & Test Attack Data Loaded (Attack Data already Combined)...")
+
+    print("CICIOT Combined Data (Train):")
+    print(ciciot_train_data.head())
+
+    print("CICIOT Combined Data (Test):")
+    print(ciciot_test_data.head())
+
+    return ciciot_train_data, ciciot_test_data, irrelevant_features
