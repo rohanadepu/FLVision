0bf8e12728320eaaf87ef2d413e2af04df9cc15b
Kevin Kostage <kskostage9457@eagle.fgcu.edu>
2024-11-07 05:24:19 -0500

    redesigns FL-DNN model and Full gan model

--- hfl/hflDiscModelServerConfig.py
+++ hfl/hflDiscModelServerConfig.py
@@ -18,13 +18,45 @@ class SaveModelStrategy(fl.server.strategy.FedAvg):
         model = create_discriminator(input_dim=input_dim)
         model.set_weights(aggregated_weights)
 
-        # Compile model for server-side training
-        model.compile(optimizer=Adam(learning_rate=0.0001), loss="categorical_crossentropy", metrics=["accuracy"])
+        # initiate optimizers
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
-        # Further train model on server-side data
-        if self.server_data is not None and self.server_labels is not None:
-            print(f"Training aggregated model on server-side data for {self.epochs} epochs...")
-            model.fit(self.server_data, self.server_labels, epochs=self.epochs, batch_size=self.batch_size, verbose=1)
+        for epoch in range(self.epochs):
+            for step, real_data in enumerate(self.x_train_ds.take(self.steps_per_epoch)):
+                # Assume real_data contains both normal and intrusive traffic
+                # Split the real_data into normal and intrusive samples
+                normal_data = real_data[
+                    real_data['Label' if self.dataset_used == "IOTBOTNET" else 'label'] == 1]  # Real normal traffic
+                intrusive_data = real_data[
+                    real_data['Label' if self.dataset_used == "IOTBOTNET" else 'label'] == 0]  # Real malicious traffic
+
+                # Generate fake data using the generator
+                noise = tf.random.normal([self.BATCH_SIZE, self.noise_dim])
+                generated_data = self.generator(noise, training=False)
+
+                # captures the discriminator’s operations to compute the gradients for adjusting its weights based on how well it classified real vs. fake data.
+                # using tape to track trainable variables during discriminator classification and loss calculations
+                with tf.GradientTape() as tape:
+                    # Discriminator outputs based on its classifications from inputted data in parameters
+                    real_normal_output = self.discriminator(normal_data, training=True)
+                    real_intrusive_output = self.discriminator(intrusive_data, training=True)
+                    fake_output = self.discriminator(generated_data, training=True)
+
+                    # Loss calculation for normal, intrusive, and fake data
+                    loss = discriminator_server_loss(real_normal_output, real_intrusive_output, fake_output)
+
+                # calculate the gradient based on the loss respect to the weights of the model
+                gradients = tape.gradient(loss, self.discriminator.trainable_variables)
+
+                # Update the model based on the gradient of the loss respect to the weights of the model
+                optimizer.apply_gradients(zip(gradients, self.discriminator.trainable_variables))
+
+                if step % 100 == 0:
+                    print(f"Epoch {epoch + 1}, Step {step}, D Loss: {loss.numpy()}")
+
+            # After each epoch, evaluate on the validation set
+            val_disc_loss = self.evaluate_validation()
+            print(f'Epoch {epoch + 1}, Validation D Loss: {val_disc_loss}')
 
         # Save the fine-tuned model
         model.save("federated_model_fine_tuned.h5")
--- hfl/hlfNIDSModelServerConfig.py
+++ hfl/hlfNIDSModelServerConfig.py
@@ -47,12 +47,64 @@ from hflNIDSModelConfig import create_CICIOT_Model, create_IOTBOTNET_Model
 class SaveModelStrategy(fl.server.strategy.FedAvg):
     def __init__(self, server_data, server_labels, epochs=5, batch_size=32, **kwargs):
         super().__init__(**kwargs)
-        self.server_data = server_data
-        self.server_labels = server_labels
-        self.epochs = epochs
+        self.data_used = dataset_used
+        self.node = node
+
+        # flags
+        self.adversarialTrainingEnabled = adversarialTrainingEnabled
+        self.DP_enabled = DP_enabled
+        self.earlyStopEnabled = earlyStopEnabled
+
+        # data
+        self.X_train_data = X_train_data
+        self.y_train_data = y_train_data
+        self.X_test_data = X_test_data
+        self.y_test_data = y_test_data
+        self.X_val_data = X_val_data
+        self.y_val_data = y_val_data
+
+        # hyperparams
         self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.steps_per_epoch = steps_per_epoch
+        # dp
+        self.num_microbatches = num_microbatches
+        self.l2_norm_clip = l2_norm_clip
+        self.noise_multiplier = noise_multiplier
+        # adversarial
+        self.adv_portion = adv_portion
+
+        # callback params
+        # early stop
+        self.metric_to_monitor_es = metric_to_monitor_es
+        self.es_patience = es_patience
+        self.restor_best_w = restor_best_w
+        # lr schedule
+        self.metric_to_monitor_l2lr = metric_to_monitor_l2lr
+        self.l2lr_factor = l2lr_patience
+        self.l2lr_patience = es_patience
+        # model checkpoint
+        self.save_best_only = save_best_only
+        self.metric_to_monitor_mc = metric_to_monitor_mc
+        self.checkpoint_mode = checkpoint_mode
+
+        # counters
+        self.roundCount = 0
+        self.evaluateCount = 0
+
+        self.callbackFunctions = []
 
     def on_fit_end(self, server_round, aggregated_weights, failures, input_dim, dataset_used, DP_enabled, regularizationEnabled, l2_alpha):
+        # increment round count
+        self.roundCount += 1
+
+        # debug print
+        print("Round:", self.roundCount, "\n")
+
+        # Record start time
+        start_time = time.time()
+
         # Create model and set aggregated weights
         if dataset_used == "IOTBOTNET":
             print("No pretrained discriminator provided. Creating a new model.")
@@ -66,17 +118,120 @@ class SaveModelStrategy(fl.server.strategy.FedAvg):
 
         model.set_weights(aggregated_weights)
 
-        # Compile model for server-side training
-        model.compile(optimizer=Adam(learning_rate=0.0001), loss="categorical_crossentropy", metrics=["accuracy"])
+        # ---         Differential Privacy Engine Model Compile              --- #
+
+        if self.DP_enabled:
+            import tensorflow_privacy as tfp
+            print("\nIncluding DP into optimizer...\n")
+
+            # Making Custom Optimizer Component with Differential Privacy
+            dp_optimizer = tfp.DPKerasAdamOptimizer(
+                l2_norm_clip=self.l2_norm_clip,
+                noise_multiplier=self.noise_multiplier,
+                num_microbatches=self.num_microbatches,
+                learning_rate=self.learning_rate
+            )
+
+            # compile model with custom dp optimizer
+            self.model.compile(optimizer=dp_optimizer,
+                               loss=tf.keras.losses.binary_crossentropy,
+                               metrics=['accuracy', Precision(), Recall(), AUC(), LogCosh()])
+
+        # ---              Normal Model Compile                        --- #
+
+        else:
+            print("\nDefault optimizer...\n")
+
+            optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+
+            self.model.compile(optimizer=optimizer,
+                               loss=tf.keras.losses.binary_crossentropy,
+                               metrics=['accuracy', Precision(), Recall(), AUC(), LogCosh()]
+                               )
+        # init callback functions based on inputs
+
+        if self.earlyStopEnabled:
+            early_stopping = EarlyStopping(monitor=self.metric_to_monitor_es, patience=self.es_patience,
+                                           restore_best_weights=self.restor_best_w)
+
+            self.callbackFunctions.append(early_stopping)
+
+        if self.lrSchedRedEnabled:
+            lr_scheduler = ReduceLROnPlateau(monitor=self.metric_to_monitor_l2lr, factor=self.l2lr_factor,
+                                             patience=self.l2lr_patience)
+
+            self.callbackFunctions.append(lr_scheduler)
+
+        if self.modelCheckpointEnabled:
+            model_checkpoint = ModelCheckpoint(f'best_model_{self.model_name}.h5', save_best_only=self.save_best_only,
+                                               monitor=self.metric_to_monitor_mc, mode=self.checkpoint_mode)
+
+            # add to callback functions list being added during fitting
+            self.callbackFunctions.append(model_checkpoint)
 
         # Further train model on server-side data
-        if self.server_data is not None and self.server_labels is not None:
+        if self.X_train_data is not None and self.y_train_data is not None:
             print(f"Training aggregated model on server-side data for {self.epochs} epochs...")
-            model.fit(self.server_data, self.server_labels, epochs=self.epochs, batch_size=self.batch_size, verbose=1)
+            history = model.fit(self.X_train_data, self.y_train_data,
+                      validation_data=(self.X_val_data, self.y_val_data),
+                      epochs=self.epochs, batch_size=self.batch_size,
+                      steps_per_epoch=self.steps_per_epoch,
+                      callbacks=self.callbackFunctions,
+                      verbose=1)
 
         # Save the fine-tuned model
         model.save("federated_model_fine_tuned.h5")
         print(f"Model fine-tuned and saved after round {server_round}.")
 
+        # Record end time and calculate elapsed time
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        # Debugging: Print the shape of the loss
+        loss_tensor = history.history['loss']
+        val_loss_tensor = history.history['val_loss']
+        # print(f"Loss tensor shape: {tf.shape(loss_tensor)}")
+        # print(f"Validation Loss tensor shape: {tf.shape(val_loss_tensor)}")
+
+        # Save metrics to file
+        logName = self.trainingLog
+        # logName = f'training_metrics_{dataset_used}_optimized_{l2_norm_clip}_{noise_multiplier}.txt'
+        self.recordTraining(logName, history, elapsed_time, self.roundCount, val_loss_tensor)
+
         # Send updated weights back to clients
         return model.get_weights(), {}
+
+    #########################################################
+    #    Metric Saving Functions                           #
+    #########################################################
+
+    def recordTraining(self, name, history, elapsed_time, roundCount, val_loss):
+        with open(name, 'a') as f:
+            f.write(f"Node|{self.node}| Round: {roundCount}\n")
+            f.write(f"Training Time Elapsed: {elapsed_time} seconds\n")
+            for epoch in range(self.epochs):
+                f.write(f"Epoch {epoch + 1}/{self.epochs}\n")
+                for metric, values in history.history.items():
+                    # Debug: print the length of values list and the current epoch
+                    print(f"Metric: {metric}, Values Length: {len(values)}, Epoch: {epoch}")
+                    if epoch < len(values):
+                        f.write(f"{metric}: {values[epoch]}\n")
+                    else:
+                        print(f"Skipping metric {metric} for epoch {epoch} due to out-of-range error.")
+                if epoch < len(val_loss):
+                    f.write(f"Validation Loss: {val_loss[epoch]}\n")
+                else:
+                    print(f"Skipping Validation Loss for epoch {epoch} due to out-of-range error.")
+                f.write("\n")
+
+    def recordEvaluation(self, name, elapsed_time, evaluateCount, loss, accuracy, precision, recall, auc, logcosh):
+        with open(name, 'a') as f:
+            f.write(f"Node|{self.node}| Round: {evaluateCount}\n")
+            f.write(f"Evaluation Time Elapsed: {elapsed_time} seconds\n")
+            f.write(f"Loss: {loss}\n")
+            f.write(f"Accuracy: {accuracy}\n")
+            f.write(f"Precision: {precision}\n")
+            f.write(f"Recall: {recall}\n")
+            f.write(f"AUC: {auc}\n")
+            f.write(f"LogCosh: {logcosh}\n")
+            f.write("\n")
